1577789cffb7dcccc6ed8565d1d725356f93d666
simieon <62874515+simieon@users.noreply.github.com>
2025-11-07 11:04:23 +0100

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -6,14 +6,14 @@ import board
 import busio
 import adafruit_character_lcd.character_lcd_i2c as character_lcd
 
-# Definiere LCD Zeilen und Spaltenanzahl.
+# define LCD rows and cols.
 lcd_columns = 16
 lcd_rows    = 2
 
-# Initialisierung I2C Bus
+# initialising an I2C Bus
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# Festlegen des LCDs in die Variable LCD
+# initialising of lcd variable
 lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows, 0x21)
 
 degree_symbol = (
@@ -29,16 +29,15 @@ degree_symbol = (
 # Store the custom character in CGRAM location 0
 lcd.create_char(0, degree_symbol)
 
-while True:
+def shutdown_lcd():
+    lcd.clear()
+    lcd.message = "Shutting down..."
+    time.sleep(1)
+    lcd.clear()
+    lcd.backlight = False
 
-    try:
-        # Hintergrundbeleuchtung einschalten
-        lcd.backlight = True
+def show_data_on_lcd(temperature: int, humidity: int):
+    # turn on Backlight
+    lcd.backlight = True
 
-        lcd.message = f"Temperatur:{1}\x00C\nFeuchtigkeit:{1}%"
-    except KeyboardInterrupt:
-        # LCD ausschalten.
-        lcd.clear()
-        lcd.message = "ERROR..."
-        time.sleep(2.0)
-        lcd.backlight = False
+    lcd.message = f"Temperatur:{temperature}\x00C\nFeuchtigkeit:{humidity}%"
